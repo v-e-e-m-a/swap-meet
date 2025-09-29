adbedbf1c99b7850fd56113a681e69f56ca7960d
Veema Jhagru <veemajhagru@gmail.com>
2025-09-29 17:33:07 -0400

    Implemented Item class in item.py according to wave 2 requirements

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,14 @@
+import uuid
+
 class Item:
-    pass
+    def __init__(self, id=None):
+        # If no id is passed through, program will make one using uuid and assign it to id
+        if id is None:
+            self.id_as_uuid = uuid.uuid4()
+            self.id = self.id_as_uuid.int
+        else:
+            self.id = id
+    
+    def get_category(self):
+        # Returns a string of the class name
+        return str(self.__class__.__name__) #implementation obtained through StackOverflow
