edd814fded0ff417688df651af5f5a8bc8ed5ac4
Olga-allgood <olga.s.orlova@gmail.com>
2025-09-29 11:28:59 -0700

    Wave one complete

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,18 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        if inventory is None:
+            self.inventory = []
+        else:
+            self.inventory = inventory[:]
+        
+
+    def add(self,item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if item not in self.inventory:
+            return False
+        else:
+            self.inventory.remove(item)
+            return item    
--- tests/unit_tests/test_wave_01.py
+++ tests/unit_tests/test_wave_01.py
@@ -2,12 +2,12 @@
 import pytest
 from swap_meet.vendor import Vendor
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_vendor_has_inventory():
     vendor = Vendor()
     assert len(vendor.inventory) == 0
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_vendor_takes_optional_inventory():
     inventory = ["a", "b", "c"]
     vendor = Vendor(inventory=inventory)
@@ -16,7 +16,7 @@ def test_vendor_takes_optional_inventory():
     assert "b" in vendor.inventory
     assert "c" in vendor.inventory
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_adding_to_inventory():
     vendor = Vendor()
     item = "new item"
@@ -27,7 +27,7 @@ def test_adding_to_inventory():
     assert item in vendor.inventory
     assert result == item
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_removing_from_inventory_returns_item():
     item = "item to remove"
     vendor = Vendor(
@@ -40,16 +40,17 @@ def test_removing_from_inventory_returns_item():
     assert item not in vendor.inventory
     assert result == item
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_removing_not_found_is_false():
-    item = "item to remove"
+    item = "d"
     vendor = Vendor(
         inventory=["a", "b", "c"]
     )
 
     result = vendor.remove(item)
+    assert result == False
 
-    raise Exception("Complete this test according to comments below.")
-    # *********************************************************************
-    # ****** Complete Assert Portion of this test **********
-    # *********************************************************************
+    # raise Exception("Complete this test according to comments below.")
+    # # *********************************************************************
+    # # ****** Complete Assert Portion of this test **********
+    # # *********************************************************************
