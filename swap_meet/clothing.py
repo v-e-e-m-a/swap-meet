182ea1073060354941ea674bf76c2c3fd57be8c2
Veema Jhagru <veemajhagru@gmail.com>
2025-09-30 16:48:04 -0400

    Completed wave 3 and wave 4

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,9 @@
-class Clothing:
-    pass
+from item import Item
+
+class Clothing(Item):
+    def __init__(self, id=None, fabric="Unknown"):
+        super().__init__(id)
+        self.fabric = fabric
+    
+    def get_category(self):
+        return "Clothing"
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -23,3 +23,29 @@ class Vendor:
                 return item
         else:
             return None
+    
+    def swap_items(self, other_vendor, my_item, their_item):
+        #Checks if items are located in the correct starting vendor
+        if my_item in self.inventory and their_item in other_vendor.inventory:
+            self.remove(my_item)
+            other_vendor.remove(their_item)
+            self.add(their_item)
+            other_vendor.add(my_item)
+            return True
+        else:
+            return False
+    
+    def swap_first_item(self, other_vendor):
+        if self.inventory == [] or other_vendor.inventory == []:
+            return False
+        else:
+            my_item = self.inventory[0]
+            their_item = other_vendor.inventory[0]
+
+            self.remove(my_item)
+            other_vendor.remove(their_item)
+
+            self.add(their_item)
+            other_vendor.add(my_item)
+
+            return True
