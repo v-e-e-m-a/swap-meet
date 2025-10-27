b10e17caebf7226b5646fd39b24229e32c29cbbd
Veema Jhagru <veemajhagru@gmail.com>
2025-10-27 16:07:23 -0400

    Removed duplicate vendor functions

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,12 +1,5 @@
 from .item import Item
 class Vendor:
-    # def __init__(self, inventory = None):
-    #     if inventory is None:
-    #         self.inventory = []
-    #     else:
-    #         self.inventory = inventory[:]
-    
-    # ternary operator /conditional expression - more concise 
     def __init__(self, inventory=None):
         self.inventory = [] if inventory is None else inventory[:]
         
@@ -20,40 +13,6 @@ class Vendor:
             return False
         self.inventory.remove(item)
         return item
-         
-    def get_by_id(self, id):  
-        for item in self.inventory:
-            if id == item.id:
-                return item
-        return None
-    
-    def swap_items(self, other_vendor, my_item, their_item):
-        if (my_item not in self.inventory 
-            or their_item not in other_vendor.inventory):
-            return False
-
-        self.remove(my_item)
-        other_vendor.add(my_item)
-        self.add(their_item)
-        other_vendor.remove(their_item)
-        return True
-        
-    def swap_first_item(self, other_vendor):
-        if not self.inventory or not other_vendor.inventory:
-            return False
-        other_vendor.inventory[0], self.inventory[0] = self.inventory[0], other_vendor.inventory[0] 
-        return True 
-    
-    def get_by_category(self, category):
-        if self.inventory == []:
-            return []
-        list_by_category =[]
-        for item in self.inventory:
-            if item.get_category() == category:
-                list_by_category.append(item) 
-        return list_by_category        
-
-        
 
     def get_by_id(self, item_id):
         for item in self.inventory:
@@ -109,11 +68,6 @@ class Vendor:
         return best_item
     
     def swap_best_by_category(self, other_vendor, my_priority, their_priority):
-        # my_item = self.get_best_by_category(their_priority)
-        # their_item = other_vendor.get_best_by_category(my_priority)
-        # result = self.swap_items(other_vendor, my_item, their_item)
-        # return result
-    
         item_to_swap = self.get_best_by_category(their_priority)
         their_item = other_vendor.get_best_by_category(my_priority)
 
